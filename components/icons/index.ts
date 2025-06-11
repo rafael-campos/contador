0e1b8437bfecc4906abc763319a2a7d32ddbd69f
rafael-campos <32342615+rafael-campos@users.noreply.github.com>
2025-06-11 17:38:16 -0300

    delete

--- components/icons/index.ts
+++
@@ -1,3 +0,0 @@
-// components/icons/index.ts
-export { default as IntertwinedHeartsIcon } from './IntertwinedHeartsIcon';
-export { default as DiamondRingIcon } from './DiamondRingIcon';
--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    '@tailwindcss/postcss': {}, // Updated from 'tailwindcss'
-    autoprefixer: {},
-  },
-};
